9b098d035db9b37fc0776d7f806c9331f4ab020a
Allen2011 <2280741023@qq.com>
2018-03-27 23:28:13 +0800

    今后会

--- src/org/git/hello/HelloGit.java
+++ src/org/git/hello/HelloGit.java
@@ -3,7 +3,7 @@ package org.git.hello;
 public class HelloGit {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Git! My darling8080");
+		System.out.println("Hello Git! My8080");
 	}
 
 }
